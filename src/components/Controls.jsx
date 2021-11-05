56165c728583e782e9a1430a97c5a2b817d85c9c
Miguel Quispe <dev@miguelquispe.com>
2021-11-04 22:34:21 -0500

    feat: Added connection for controls

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,24 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { moveDown, moveLeft, moveRight, rotate } from "../slices/gameSlice";
+
 const Controls = (props) => {
+  const dispatch = useDispatch();
+  const isRunning = useSelector(state => state.game.isRunning)
+
   return (
     <div className="controls">
       {/* left */}
       <button className="control-button" onClick={(e) => {
-
+        dispatch(moveLeft())
       }}>Left</button>
 
       {/* right */}
       <button className="control-button" onClick={(e) => {
-
+        dispatch(moveRight())
       }}>Right</button>
 
       {/* rotate */}
       <button className="control-button" onClick={(e) => {
-
+        dispatch(rotate);
       }}>Rotate</button>
 
       {/* down */}
       <button className="control-button" onClick={(e) => {
-
+        dispatch(moveDown())
       }}>Down</button>
     </div>
   );
